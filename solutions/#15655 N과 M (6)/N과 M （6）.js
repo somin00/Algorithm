461b596a2d59d01b4b0463d7fc5fe52eb1d1e86d
somin <61578822+somin00@users.noreply.github.com>
2023-10-10 13:03:36 +0900

    [Silver III 15655] Title: N과 M (6), Time: 120 ms, Memory: 9604 KB -NKLCBHub

---
+++ solutions/#15655 N과 M (6)/N과 M （6）.js
@@ -0,0 +1,37 @@
+const [[N, M], numbersArr] = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item) => item.split(" ").map(Number));
+
+const numbers = numbersArr.sort((a, b) => a - b);
+
+function solution(N, M) {
+  const seq = [];
+  const visited = new Array(N).fill(false);
+  let result = "";
+
+  dfs(0);
+
+  function dfs(x) {
+    if (x === M) {
+      result += seq.join(" ") + "\n";
+      return;
+    } else {
+      for (let i = 0; i < N; i++) {
+        const last = seq.at(-1) ?? 0;
+        if (!visited[i] && numbers[i] > last) {
+          seq.push(numbers[i]);
+          visited[i] = true;
+          dfs(x + 1);
+          seq.pop();
+          visited[i] = false;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+console.log(solution(N, M));
