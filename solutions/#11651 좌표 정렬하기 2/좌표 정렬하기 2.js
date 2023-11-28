633ce31848b40268bb774307b23b6b5195f4cfd8
somin <61578822+somin00@users.noreply.github.com>
2023-11-28 14:30:50 +0900

    [Silver V 11651] Title: 좌표 정렬하기 2, Time: 496 ms, Memory: 56828 KB -NKLCBHub

---
+++ solutions/#11651 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,30 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const count = +input[0];
+
+let arr = [];
+
+for (let i = 1; i <= count; i++) {
+  arr.push(input[i].split(" ").map((item) => Number(item)));
+}
+
+arr.sort((a, b) => {
+  if (a[1] === b[1]) {
+    return a[0] - b[0];
+  } else {
+    return a[1] - b[1];
+  }
+});
+
+let ans = [];
+
+for (let i = 0; i < count; i++) {
+  let answer = arr[i].join(" ");
+  ans.push(answer);
+}
+
+console.log(ans.join("\n"));
