4a362b8fff827dfd81c292b6c4d260606482bcfe
somin <61578822+somin00@users.noreply.github.com>
2023-12-17 15:17:19 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -0,0 +1,27 @@
+function solution(sequence, k) {
+    const answer = [];
+    
+    let startIdx=0, endIdx=1; 
+    let sum=sequence[0];
+    let minDist=Infinity; 
+
+    while(startIdx<=sequence.length-1  && endIdx<=sequence.length){
+        if(sum<k){
+            sum+=sequence[endIdx++]; 
+        }else if(sum>k){
+            sum-=sequence[startIdx++]; 
+        }else if(sum===k){
+            sum-=sequence[startIdx]; 
+            minDist=Math.min(minDist, endIdx-startIdx-1); 
+            answer.push({dist: minDist, val:[startIdx++, endIdx]});  
+        }
+    }
+    
+    if(answer.length===1) {
+        const [x, y]=answer[0].val; 
+        return [x, y-1]; 
+    }
+    
+    const [x,y]= answer.filter(item=>item.dist===minDist).map(({val})=>val).sort((a,b)=>a[0]-b[0])[0];
+    return [x, y-1]; 
+}
