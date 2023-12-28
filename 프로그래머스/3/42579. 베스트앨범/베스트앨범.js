04e1ed3d819723a8e7f1e99925eb047f6e6c6f6a
somin <61578822+somin00@users.noreply.github.com>
2023-12-28 16:15:19 +0900

    [level 3] Title: 베스트앨범, Time: 0.25 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,39 @@
+function solution(genres, plays) {
+    const answer=[]; 
+    const map={}
+    
+    for(let i=0; i<genres.length; i++){
+        if(map[genres[i]]){
+            const prevObj=map[genres[i]]
+            map[genres[i]]={
+                sum: prevObj.sum+plays[i], 
+                arr: [...(prevObj.arr), [i, plays[i]]]
+            }
+        }else{
+            map[genres[i]]={sum: plays[i], arr:[[i, plays[i]]]}
+        }
+   }
+    
+    const sortedgenre=Object.keys(map).sort((a,b)=>map[b].sum-map[a].sum); 
+    
+    let totalCount=sortedgenre.length*2; 
+    
+    for(let i=0; i<sortedgenre.length; i++){
+        (map[sortedgenre[i]].arr).sort((a,b)=>{
+            if(a[1]===b[1]) return a[0]-b[0]; 
+            else return b[1]-a[1]; 
+        }); 
+
+        if((map[sortedgenre[i]].arr).length===1) {
+            answer.push(map[sortedgenre[i]].arr[0][0]); 
+            totalCount-=1; 
+        }
+        else {
+            for(let j=0; j<2; j++){
+                answer.push(map[sortedgenre[i]].arr[j][0]); 
+            }
+        }
+    }
+    return answer.slice(0, totalCount); 
+    
+}
