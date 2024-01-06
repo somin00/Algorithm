296c6dbf87cd0e3e99ebd5c5435ff51200d034ee
somin <61578822+somin00@users.noreply.github.com>
2024-01-07 00:18:32 +0900

    [Bronze II] Title: 시험 감독, Time: 452 ms, Memory: 88092 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.js
+++ 백준/Bronze/13458. 시험 감독/시험 감독.js
@@ -9,20 +9,16 @@ const people = input.shift().split(" ").map(Number);
 const [b, c] = input[0].split(" ").map(Number);
 
 let answer = 0;
-const obj = {};
+
 function getCount(total) {
   let count = 1;
-  total -= b;
   if (total > 0) {
     count += Math.ceil(total / c);
   }
   return count;
 }
 for (let i = 0; i < n; i++) {
-  if (obj[people[i]]) answer += obj[people[i]];
-  else {
-    answer += getCount(people[i]);
-  }
+  answer += getCount(people[i] - b);
 }
 
 console.log(answer);
