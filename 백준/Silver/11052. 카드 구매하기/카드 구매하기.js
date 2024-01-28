505f4171bf5b8a54e3791bfb1b50b810b10a53e6
somin <61578822+somin00@users.noreply.github.com>
2024-01-28 22:57:09 +0900

    [Silver I] Title: 카드 구매하기, Time: 188 ms, Memory: 9664 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
@@ -6,15 +6,11 @@ const input = require("fs")
 
 const n = +input[0];
 const arr = input[1].split(" ").map(Number);
-const dp = Array.from({ length: n + 1 }, () => new Array(n + 1).fill(0));
-dp[1][0] = arr[0];
+const dp = Array.from({ length: n + 1 }, () => 0);
 
-for (let i = 2; i < dp.length; i++) {
-  for (let j = 0; j < i; j++) {
-    for (let k = i - j - 1; k >= 0; k--) {
-      dp[i][j] = Math.max(dp[i - j - 1][k] + arr[j], dp[i][j]);
-    }
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    dp[i] = Math.max(dp[i], dp[i - j] + arr[j - 1]);
   }
 }
-
-console.log(Math.max(...dp[n]));
+console.log(dp[n]);
