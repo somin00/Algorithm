1f23557326be1707ede753d02dc013f404a4a1a4
somin <61578822+somin00@users.noreply.github.com>
2024-12-14 22:48:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.01 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,6 +1,11 @@
 function solution(participant, completion) {
-    participant.sort(); 
-    completion.sort(); 
+    const pMap=new Map(); 
     
-    return participant.find((item, idx)=>item!==completion[idx])
+    participant.map((name)=>pMap.set(name, (pMap.get(name)|0)+1))
+    completion.map((name)=>pMap.set(name, (pMap.get(name)|0)-1))
+    
+    for(let [key, value] of pMap){
+        if(value>0) return key
+    }
+
 }
